910adbf478af055a051a76a105a5fe8b4fd1766c
McKay Broderick <mckay.db@gmail.com>
2018-06-14 22:06:24 -0600

    Got it sending the email.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import Prelude (Show, Eq, Bool(False), Maybe(Just, Nothing), IO, print, putStrLn, pure, sequence, fmap, foldr, show, (<$>), ($), (>>=), (*), (=<<), (/=), (.), (&&))
+import Prelude (Show, Eq, Bool(False), Maybe(Just, Nothing), IO, print, putStrLn, pure, sequence, fmap, foldr, show, (>), (<$>), ($), (>>=), (*), (=<<), (/=), (.), (&&))
 import Control.Lens ((.~), (&), (^?))
-import Control.Monad (forever)
+import Control.Monad (forever, when)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.State.Lazy (StateT, runStateT, get, put)
 import Control.Concurrent (threadDelay)
@@ -10,7 +10,7 @@ import Data.HashMap.Lazy (HashMap, singleton, unions, (!))
 import Data.Semigroup ((<>))
 import Data.List (find, (\\))
 import Data.List.NonEmpty (fromList)
-import Data.Text (Text, pack)
+import Data.Text (Text, length, pack)
 import Data.Aeson (decode)
 import Data.Maybe (fromMaybe)
 import qualified Data.ByteString.Char8 (ByteString, takeWhile, dropWhile, isInfixOf, init, pack)
@@ -61,21 +61,25 @@ handleSites Ksl (SearchTerm s) = do
       putStrLn "Couldn't find response body!"
       pure []
 
-handleSites FacebookMarketplace (SearchTerm s) = do
+handleSites FacebookMarketplace (SearchTerm _) = do
   -- print $ "Checking Facebook Marketplace: " <> s
   pure []
 
-handleSites LetGo (SearchTerm s) = do
+handleSites LetGo (SearchTerm _) = do
   -- print $ "Checking LetGo: " <> s
   pure []
 
-handleSites OfferUp (SearchTerm s) = do
+handleSites OfferUp (SearchTerm _) = do
   -- print $ "Checking OfferUp: " <> s
   pure []
 
 createMail :: MailAddress -> Text -> Mail () ()
-createMail addr content =
-  mail [personalization (fromList [addr])] addr "Possible Deal" (fromList [mailContentText content])
+createMail addr content = mail to' from' subject' content'
+  where
+    from' = MailAddress "arcade.dealz@gmail.com" "Arcade Finder"
+    subject' = "Possible Deals"
+    content' = fromList [mailContentText content]
+    to' = [personalization (fromList [addr])]
 
 diffListings :: Site -> [Listing] -> [Listing] -> [Listing]
 -- If one of these is empty, then something has gone wrong, or it's the first call, so we can't get a good diff
@@ -85,8 +89,8 @@ diffListings _ a b = b \\ a
 
 -- "https://www.ksl.com/classifieds/listing/id-of-listing"
 getMailContent :: Site -> [Listing] -> Text
-getMailContent Ksl listings = foldr (\a b -> a <> b <> " <br>\n ") "" $ fmap f listings
-  where f (KslListing id _ _ _ _ _ _ _ price title _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _) = "https://ksl.com/classifieds/listing/" <> pack (show id) <> " <br>\n " <> title <> " <br>\n " <> pack (show price)
+getMailContent Ksl listings = foldr (<>) "\n" $ fmap f listings
+  where f (KslListing id _ _ _ _ _ _ _ price title _ _ _ _ _ _ name homePhone _ _ _ _ _ _ _ _ _ _ _ _) = title <> "\n" <> pack (show price) <> "\n" <> "ksl.com/classifieds/listing/" <> pack (show id) <> "\n" <> name <> "\n" <> homePhone <> "\n"
 getMailContent FacebookMarketplace _ = "Facebook Marketplace Content"
 getMailContent LetGo _ = "LetGo Content"
 getMailContent OfferUp _ = "OfferUp Content"
@@ -100,28 +104,18 @@ getListings (sendgridApiKey, mailAddr) = forever $ do
   -- let oldOuListings = prevListings ! OfferUp
   -- let oldLgListings = prevListings ! LetGo
 
-  newKslListings <- liftIO $ handleSites Ksl $ SearchTerm "arcade"
-  newFbmpListings <- liftIO $ handleSites FacebookMarketplace $ SearchTerm "arcade"
-  newOuListings <- liftIO $ handleSites OfferUp $ SearchTerm "arcade"
-  newLgListings <- liftIO $ handleSites LetGo $ SearchTerm "arcade"
+  newKslListings <- liftIO $ handleSites Ksl $ SearchTerm "game"
+  newFbmpListings <- liftIO $ handleSites FacebookMarketplace $ SearchTerm "game"
+  newOuListings <- liftIO $ handleSites OfferUp $ SearchTerm "game"
+  newLgListings <- liftIO $ handleSites LetGo $ SearchTerm "game"
 
   -- Collect the diff listings into email content
-  -- liftIO $ putStrLn "New Ksl Listings: "
-  -- liftIO $ print $ diffListings Ksl oldKslListings newKslListings
   let diffKsl = diffListings Ksl oldKslListings newKslListings
   let kslContent = getMailContent Ksl diffKsl
-  case diffKsl of
-    [] -> liftIO $ print "No changes."
-    x -> liftIO $ do
-      print "Old Listings: "
-      print oldKslListings
-      print "New Listings: "
-      print newKslListings
-      print "Diff Listings: "
-      print diffKsl
-      print kslContent
 
-  -- statusCode <- liftIO $ sendMail sendgridApiKey (createMail mailAddr kslContent)
+  liftIO $ when (length kslContent > 1) $ do
+    statusCode <- sendMail sendgridApiKey (createMail mailAddr kslContent)
+    print ("Sent email: " <> show statusCode)
 
   put $ unions [ singleton Ksl newKslListings
                , singleton FacebookMarketplace newFbmpListings
